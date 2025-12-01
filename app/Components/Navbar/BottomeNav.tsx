235615992339c54b323f0ad7228b42581f8b0685
danial137 <danial79fakhrabadi@gmail.com>
2025-12-01 12:20:48 +0330

    feat: add footer and navbar components; update styles and dependencies

---
+++ app/Components/Navbar/BottomeNav.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BottomeNav = () => {
+  return (
+    <div>BottomeNav</div>
+  )
+}
+
+export default BottomeNav
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,23 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Merienda, Unbounded } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
+
+const unbounded = Unbounded({
+  variable: "--font-unbounded",
+  subsets: ["latin"],
+  display:"swap",
+});
+const merienda = Merienda({
+  variable: "--font-merienda",
+  subsets: ["latin"],
+  display: "swap",
+});
+
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
